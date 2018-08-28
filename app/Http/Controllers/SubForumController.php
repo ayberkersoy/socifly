f29f7ffc12e0b15ac7bd784a77b4634e59beb00c
Ayberk Ersoy <ayberkersoy@gmail.com>
2018-08-28 21:41:38 +0300

    Add delete sub forum function

--- app/Http/Controllers/SubForumController.php
+++ app/Http/Controllers/SubForumController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\SubForum;
 use Illuminate\Http\Request;
 
 class SubForumController extends Controller
 {
-    //
+    public function destroy(SubForum $subForum)
+    {
+        $subForum->delete();
+        return back();
+    }
 }
